b36db347ec53387f6093dfed28c38038499019b2
aaronreilly00 <you@example.com>
2021-03-25 08:49:07 +0000

    final updates for jmDNS

--- src/main/java/jmdns/ServiceRegistration.java
+++ src/main/java/jmdns/ServiceRegistration.java
@@ -14,17 +14,15 @@ public class ServiceRegistration {
 
         try {
             // Create a JmDNS instance
-            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+        	JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
 
             // Register a service
-            ServiceInfo serviceInfo = ServiceInfo.create("_http._tcp.local.", "example", 8000, "path=index.html");
-            jmdns.registerService(serviceInfo);
-            
+            ServiceInfo guiService = ServiceInfo.create("_http._tcp.local.", "livestock", 8000, "path=index.html");
+            jmdns.registerService(guiService);
             // Wait a bit
-            Thread.sleep(20000);
+            Thread.sleep(25000);
 
-            // Unregister all services
-            //jmdns.unregisterAllServices();
+            jmdns.unregisterAllServices();
             
 
         } catch (IOException e) {
